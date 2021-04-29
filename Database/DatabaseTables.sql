ac5cb5f72009c72c7d53e20f5cb125f0396909fc
bb554C <bairdbuenaventura@gmail.com>
2021-04-29 15:08:11 +0800

    Update DatabaseTables.sql

--- Database/DatabaseTables.sql
+++ Database/DatabaseTables.sql
@@ -109,4 +109,82 @@ GO
 ALTER DATABASE [YearPlanner] SET  READ_WRITE 
 GO
 
+USE [YearPlanner]
+GO
+
+/****** Object:  Table [dbo].[Email_Table]    Script Date: 04/29/2021 3:06:32 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Email_Table](
+	[EmailID] [int] IDENTITY(1,1) NOT NULL,
+	[Email] [varchar](200) NOT NULL,
+ CONSTRAINT [PK_Email_Table] PRIMARY KEY CLUSTERED 
+(
+	[EmailID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+USE [YearPlanner]
+GO
+
+/****** Object:  Table [dbo].[Gender_Table]    Script Date: 04/29/2021 3:07:13 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Gender_Table](
+	[GenderID] [int] IDENTITY(1,1) NOT NULL,
+	[GenderType] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_Gender_Table] PRIMARY KEY CLUSTERED 
+(
+	[GenderID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+USE [YearPlanner]
+GO
+
+/****** Object:  Table [dbo].[User_Table]    Script Date: 04/29/2021 3:07:31 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[User_Table](
+	[UserID] [int] IDENTITY(1,1) NOT NULL,
+	[EmailID_FK] [int] NOT NULL,
+	[GenderID_FK] [int] NULL,
+	[FirstName] [varchar](100) NULL,
+	[LastName] [varchar](100) NULL,
+	[Password] [varchar](200) NOT NULL,
+	[Birthday] [date] NOT NULL,
+ CONSTRAINT [PK_User_Table] PRIMARY KEY CLUSTERED 
+(
+	[UserID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[User_Table]  WITH CHECK ADD  CONSTRAINT [FK_User_Table_Email_Table] FOREIGN KEY([EmailID_FK])
+REFERENCES [dbo].[Email_Table] ([EmailID])
+GO
+
+ALTER TABLE [dbo].[User_Table] CHECK CONSTRAINT [FK_User_Table_Email_Table]
+GO
+
+ALTER TABLE [dbo].[User_Table]  WITH CHECK ADD  CONSTRAINT [FK_User_Table_User_Table] FOREIGN KEY([GenderID_FK])
+REFERENCES [dbo].[Gender_Table] ([GenderID])
+GO
+
+ALTER TABLE [dbo].[User_Table] CHECK CONSTRAINT [FK_User_Table_User_Table]
+GO
 
