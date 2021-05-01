7681283cde6d7fff4ad1075b76b3908061072c97
bb554C <bairdbuenaventura@gmail.com>
2021-05-01 17:51:14 +0800

    Update StoredProcedures.sql

--- Database/StoredProcedures.sql
+++ Database/StoredProcedures.sql
@@ -264,14 +264,24 @@ GO
 CREATE PROCEDURE UpdateObjective
 	@ObjectiveID int,
 	@UserID_FK int,
-	@RepetitionID_FK int,
-	@PriorityID_FK int,
+	@RepetitionType varchar(100),
+	@PriorityType varchar(100),
 	@ObjectiveTitle varchar(100),
 	@ObjectiveDescription varchar(MAX),
 	@ObjectiveDate date
 AS
 BEGIN
 	SET NOCOUNT ON;
+	DECLARE @RepetitionID_FK int, @PriorityID_FK int;
+
+	SELECT @RepetitionID_FK = RT.RepetitionID
+	FROM Repetition_Table AS RT
+	WHERE RT.RepetitionType = @RepetitionType
+
+	SELECT @PriorityID_FK = PT.PriorityID
+	FROM Priority_Table AS PT
+	WHERE PT.PriorityType = @PriorityType
+
 	UPDATE Objective_Table
 	SET RepetitionID_FK = @RepetitionID_FK,
 		PriorityID_FK = @PriorityID_FK,
